632b8ba4668b726b82234b7bb858760a8974b8c7
Cristiano Cunha <Contato@cristianoprogramador.com>
2022-10-21 08:48:27 -0300

    Cria a base do producer

--- Example/Producer/index.js
+++ Example/Producer/index.js
@@ -1,1 +1,26 @@
-console.log("Producer")
+const { Kafka } = require('kafkajs');
+const { faker } = require('@faker-js/faker');
+
+const clientId = process.env.CLIENT_ID;
+const broker = process.env.BROKER_01;
+const numberOfMessages = process.env.MESSAGES;
+const topic = process.env.TOPIC;
+
+const kafka = new Kafka({
+    clientId: clientId,
+    brokers: [broker],
+})
+
+const producer = kafka.producer()
+producer.connect()
+
+for (let index = 0; index < numberOfMessages; index++) {
+    const message = { value: faker.name.fullName() };
+    //console.log(message);
+    producer.send({
+        topic: topic,
+        messages: [message],
+    });
+}
+
+producer.disconnect()
