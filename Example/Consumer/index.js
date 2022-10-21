56bc9f267e5fef74eefc99fed3ea2397ce627f07
Cristiano Cunha <Contato@cristianoprogramador.com>
2022-10-21 08:55:47 -0300

    Estrutura base do consumidor

--- Example/Consumer/index.js
+++ Example/Consumer/index.js
@@ -1,1 +1,24 @@
-console.log("Consumer")
+const { Kafka } = require('kafkajs');
+
+const clientId = process.env.CLIENT_ID;
+const broker = process.env.BROKER_01;
+const topic = process.env.TOPIC;
+
+const kafka = new Kafka({
+    clientId: clientId,
+    brokers: [broker],
+    ssl: false,
+})
+
+const consumer = kafka.consumer({ groupId: 'example-group' })
+
+consumer.connect().then();
+consumer.subscribe({ topic: topic, fromBeginning: true }).then();
+
+consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+        console.log({
+            value: message.value.toString(),
+        })
+    },
+}).then();
